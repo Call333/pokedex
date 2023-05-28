3279863f89d29a77745b393a57b2865eb3f24c34
Call333 <cam4@aluno.ifal.edu.br>
2023-05-27 21:02:07 -0300

    as

--- src/services/firebase/firestore/firestore.js
+++ src/services/firebase/firestore/firestore.js
@@ -3,23 +3,26 @@ import { Button, Text, View } from "react-native";
 import { getFirestore, setDoc, doc } from 'firebase/firestore'
 
 import {app} from './../../firebase/autentication/Auth'
+import { useEffect } from "react";
 
 const db = getFirestore(app)
 
 export default function TesteFire(){
-
-    setDoc(doc(db, "pokemons", "IO"), {
-        name: "Bulbasaur",
-        number: "#001",
-        types: ['grass', 'poison']
-      });
+    
+    const addPoke = () => {
+        setDoc(doc(db, "pokemons1", "IO"), {
+            name: "Bulbasaur",
+            number: "#056",
+            types: ['grass', 'poison']
+          });
+    }
 
     return(
         <View>
             <Text>
                 OI
             </Text>
-            <Button title="Firebase Enviar" onPress={setDoc}></Button>
+            <Button title="Firebase Enviar" onPress={addPoke}></Button>
         </View>
     )
 }
